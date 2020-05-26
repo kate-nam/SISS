6f3a2fa06fcce717666726f257cf2ea75f948916
kate-nam <59225678+kate-nam@users.noreply.github.com>
2020-05-26 22:59:08 +0900

    Update w3-3.c

--- w3-3.c
+++ w3-3.c
@@ -5,22 +5,21 @@
 //  Created by KMNAM on 2020/05/13.
 //  Copyright © 2020 KMNAM. All rights reserved.
 //
-
 #include <stdio.h>
 
-int main(void) {
-    int a[5], i;
-    for(i=0; i<5; i++){
+int main() {
+    int a[5],i;
+    for(i=0;i<5;i++){
         scanf("%d", &a[i]);
     }
-    int max=a[0], min=a[0], sum=0;
-    for(i=0; i<5; i++){
-        if(max<a[i])
-            max=a[i];
+    int min, max, sum=0;
+    min=max=a[0];
+    for(i=0;i<5;i++){
         if(min>a[i])
             min=a[i];
+        else if(max<a[i])
+            max=a[i];
         sum+=a[i];
     }
-    printf("%d\n%d\n%d\n", max, min, sum);
-    return 0;
+    printf("%d\n%d\n%d\n", min, max, sum);
 }
