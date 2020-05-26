63a502fcfcc857392f723e8b33b6c75b8455314c
kate-nam <59225678+kate-nam@users.noreply.github.com>
2020-05-26 22:45:09 +0900

    Update w3-2.c

--- w3-2.c
+++ w3-2.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
-int main(void){
-    int a[3][3];
-    int row, col, time=1;
+
+int main() {
+    int a[3][3], i, j, k=1;
     printf("original matrix\n");
-    for(row=0; row<3; row++){
-        for(col=0; col<3; col++){
-            printf("%d\t", a[row][col]=time*2);
-            time++;
-            if(col==2){
-                printf("\n");
-            }
+    for(i=0; i<3;i++){
+        for(j=0;j<3;j++){
+            a[i][j]=2*k;
+            printf("%d\t", a[i][j]);
+            k++;
         }
+        printf("\n");
     }
     printf("transpose matrix\n");
-    time=0;
-    for(row=0; row<3; row++){
-        for(col=0; col<3; col++){
-            if((row<col) || (row>col) )
-                printf("%d\t", a[col][row]);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(i==j)
+                printf("%d\t", a[i][j]);
             else
-                printf("%d\t", a[row][col]);
-            if(col==2)
-                printf("\n");
+                printf("%d\t", a[j][i]);
         }
+        printf("\n");
     }
-    return 0;
-}
+    
+ }
 
