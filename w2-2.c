5a3009ba35d8d42307e4ed1d92c9c0c9ebce9d69
KMNAM <kate.k.nam@gmail.com>
2020-05-26 22:03:31 +0900

    no message

--- w2-2.c
+++ w2-2.c
@@ -8,16 +8,14 @@
 
 #include <stdio.h>
 
-int main() {
-    double year, sec, a, b;
-    int day, hour, min;
-    year = 365.2422;
-    day= (int)(year); // 365.2422의 소수점을 버리고 정수로 변환
-    a= (year-day)*24; // 0.2422일을 5.81288시간으로 바꿈
-    hour= (int)(a); // 정수형으로 바꿈. 5시간
-    b= (a - hour) * 60; //0.81288시간을 48.768분으로 바꿈
-    min= (int)(b); // 정수형으로 바꿈. 48분
-    sec= (b-min)*60; //0.768분을 46.08초로 바꿈
-    printf("%.4f일은 %d일 %d시간 %d분 %.2f초입니다.\n", year, day, hour, min, sec);
-    return 0;
+int main(void) {
+    double year= 365.2422, hour, min, sec;
+    hour= year-(int)year;
+    year= (int)year;
+    hour= hour*24;
+    min= hour-(int)hour;
+    min= min*60;
+    sec= min-(int)min;
+    sec= sec*60;
+    printf("%d일 %d시간 %d분 %6.2f초", (int)year,(int)hour,(int)min,sec);
 }
