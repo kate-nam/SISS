6618ad353b539f99d70dd9cdd2cbe77f43152c74
kate-nam <59225678+kate-nam@users.noreply.github.com>
2020-05-26 22:25:06 +0900

    Update w3-1.c

--- w3-1.c
+++ w3-1.c
@@ -1,29 +1,15 @@
-//
-//  main.c
-//  3-1
-//
-//  Created by KMNAM on 2020/05/13.
-//  Copyright © 2020 KMNAM. All rights reserved.
-//
 
 #include <stdio.h>
-int sum(int a, int b);
-int mul(int a, int b);
-int main(void) {
-    int x, y;
-    int result=0;
-    scanf("%d%d", &x, &y);
-    result= sum(x,y);
-    printf("%d\n", result);
-    result= mul(x,y);
-    printf("%d\n", result);
-    return 0;
-}
 int sum(int a, int b){
-    int c=a+b;
-    return c;
+    return a+b;
 }
 int mul(int a, int b){
-    int c=a*b;
-    return c;
+    return a*b;
+}
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("%d\n%d\n4", sum(a,b), mul(a,b));
+    return 0;
 }
